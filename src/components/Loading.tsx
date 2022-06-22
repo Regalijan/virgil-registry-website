22116160ec72a71a5ab603beb6cc568f1a37f0eb
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2022-06-22 22:00:32 +0000

    Create loading component

---
+++ src/components/Loading.tsx
@@ -0,0 +1,9 @@
+import { Container, Spinner } from "@chakra-ui/react";
+
+export default function () {
+  return (
+    <Container paddingTop="25vh">
+      <Spinner size="xl" />
+    </Container>
+  );
+}
