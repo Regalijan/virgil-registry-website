dda04ac59a93ca610d0cfaa8c7fe7aea5293e63a
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2022-06-22 21:25:51 +0000

    Create initial theme

---
+++ src/theme.ts
@@ -0,0 +1,17 @@
+import { extendTheme, type ThemeConfig } from "@chakra-ui/react";
+
+const config: ThemeConfig = {
+  initialColorMode: "dark",
+  useSystemColorMode: false,
+};
+
+const fontString = `"Plus Jakarta Sans", -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;`;
+
+const fonts = {
+  heading: fontString,
+  body: fontString,
+};
+
+const theme = extendTheme({ config, fonts });
+
+export default theme;
