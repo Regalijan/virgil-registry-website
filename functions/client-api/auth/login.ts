1f856202243effbea6d86c731b8a8506f98a9c65
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2022-06-24 05:15:06 +0000

    Change redirect_uri

--- functions/client-api/auth/login.ts
+++ functions/client-api/auth/login.ts
@@ -1,13 +1,13 @@
 import { Context } from "../../..";
 
-export async function onRequestGet(context: Context) {
+export async function onRequestGet(context: Context): Promise<Response> {
   const { env, request } = context;
   const { hostname, protocol } = new URL(request.url);
   return Response.redirect(
     `https://discord.com/api/oauth2/authorize?client_id=${
       env.DISCORD_CLIENT
     }&redirect_uri=${encodeURIComponent(
-      `${protocol}//${hostname}/client-api/auth/callback`
+      `${protocol}//${hostname}/login`
     )}&response_type=code&scope=identify`
   );
 }
