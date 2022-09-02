625afb805c190cb6f068475f4c86f4803632336d
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2022-09-02 02:53:59 +0000

    Create connect endpoint

---
+++ functions/client-api/linking/connect.ts
@@ -0,0 +1,61 @@
+export async function onRequestPost(
+  context: EventContext<{ [k: string]: string }, string, { [k: string]: any }>
+) {
+  const { data, env } = context;
+
+  if (!data.body?.code || !data.body.verifier)
+    return new Response(
+      JSON.stringify({ error: 'Missing "code" or "verifier"' }),
+      {
+        headers: {
+          "content-type": "application/json",
+        },
+        status: 400,
+      }
+    );
+
+  const jwtFetch = await fetch("https://apis.roblox.com/oauth/v1/token", {
+    headers: {
+      authorization: `Basic ${btoa(env.RBX_ID + ":" + env.RBX_SECRET)}`,
+      "content-type": "application/x-www-form-urlencoded",
+    },
+  });
+
+  if (!jwtFetch.ok)
+    return new Response(JSON.stringify({ error: "Failed to redeem code" }), {
+      headers: {
+        "content-type": "application/json",
+      },
+      status: 500,
+    });
+
+  const { id_token }: { id_token?: string } = await jwtFetch.json();
+
+  if (!id_token)
+    return new Response(
+      JSON.stringify({ error: "Failed to verify information from Roblox" }),
+      {
+        headers: {
+          "content-type": "application/json",
+        },
+        status: 500,
+      }
+    );
+
+  const decodedToken: { [k: string]: string } = JSON.parse(
+    atob(id_token.replaceAll("-", "+").replaceAll("_", "/"))
+  );
+  const verifyKV = env.VERIFICATIONS as unknown as KVNamespace;
+
+  await verifyKV.put(
+    data.user.id,
+    JSON.stringify({
+      id: parseInt(decodedToken.sub),
+      username: decodedToken.name,
+    })
+  );
+
+  return new Response(null, {
+    status: 204,
+  });
+}
