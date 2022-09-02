196bbb5419739e7e3e7738010b85d41ee0799a11
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2022-09-02 01:30:21 +0000

    Move top level auth handlng to middleware

--- functions/client-api/_middleware.ts
+++ functions/client-api/_middleware.ts
@@ -7,16 +7,24 @@ function makeError(error: string, status: number): Response {
   });
 }
 
-export async function onRequest(context: EventContext<{[k:string]:string},string,{[k:string]:any}>) {
+export async function onRequest(
+  context: EventContext<{ [k: string]: string }, string, { [k: string]: any }>
+) {
   const { request } = context;
   const sessionStore = context.env.SESSIONS as unknown as KVNamespace;
   const authToken = request.headers.get("authorization");
 
   if (authToken) {
-    const tokenHash = await crypto.subtle.digest("SHA-512", new TextEncoder().encode(authToken))
-    const session = await sessionStore.get(Array.from(new Uint8Array(tokenHash)).map((b) => b.toString(16).padStart(2, "0")).join(""))
-    if (session)
-      context.data.user = JSON.parse(session);
+    const tokenHash = await crypto.subtle.digest(
+      "SHA-512",
+      new TextEncoder().encode(authToken)
+    );
+    const session = await sessionStore.get(
+      Array.from(new Uint8Array(tokenHash))
+        .map((b) => b.toString(16).padStart(2, "0"))
+        .join("")
+    );
+    if (session) context.data.user = JSON.parse(session);
   }
 
   if (request.method === "POST") {
@@ -26,7 +34,7 @@ export async function onRequest(context: EventContext<{[k:string]:string},string
     try {
       context.data.body = await request.json();
     } catch {
-      return makeError("Invalid JSON", 400)
+      return makeError("Invalid JSON", 400);
     }
   }
 
