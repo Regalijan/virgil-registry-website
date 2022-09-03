f91ddaa7a6aafd27e1ee45f1415ed51bbe611735
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2022-09-03 08:00:32 +0000

    Create unverify endpoint

--- functions/client-api/linking/current.ts
+++ functions/client-api/linking/current.ts
@@ -1,3 +1,47 @@
+export async function onRequestDelete(
+  context: EventContext<
+    { [k: string]: string } & { VERIFICATIONS: KVNamespace },
+    string,
+    { [k: string]: any }
+  >
+) {
+  const verifyKV = context.env.VERIFICATIONS;
+  const verifyData = await verifyKV.get(context.data.user.id);
+
+  if (!verifyData)
+    return new Response(JSON.stringify({ error: "You are not verified" }), {
+      headers: {
+        "content-type": "application/json",
+      },
+      status: 404,
+    });
+
+  const data = JSON.parse(verifyData);
+  const reverseData: string[] = JSON.parse(
+    (await verifyKV.get(data.id.toString())) ?? "[]"
+  );
+  const reverseIndex = reverseData.findIndex(
+    (id) => id === context.data.user.id
+  );
+
+  if (reverseIndex === -1)
+    return new Response(null, {
+      status: 204,
+    });
+
+  reverseData.splice(reverseIndex, 1);
+
+  if (reverseData.length) {
+    await verifyKV.put(data.id.toString(), JSON.stringify(reverseData));
+  } else {
+    await verifyKV.delete(data.id.toString());
+  }
+
+  return new Response(null, {
+    status: 204,
+  });
+}
+
 export async function onRequestGet(
   context: EventContext<
     { [k: string]: string } & { VERIFICATIONS: KVNamespace },
