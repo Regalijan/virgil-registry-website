74409dcd5e7ab949208c4a402666b362f90744f2
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2022-09-04 17:30:44 +0000

    Increase size of logout button

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -162,8 +162,8 @@ export default function () {
                   </Text>
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
+                    width="24"
+                    height="24"
                     fill="currentColor"
                     viewBox="0 0 16 16"
                     style={{ display: userData.id ? "block" : "none" }}
@@ -211,8 +211,8 @@ export default function () {
           </Text>
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
+            width="24"
+            height="24"
             fill="currentColor"
             viewBox="0 0 16 16"
             style={{ display: userData.id ? "block" : "none" }}
