94aca9728592d2f6fbb2e8c254e85cc50e3cdeaf
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2022-09-05 20:43:41 +0000

    Make logo centered on mobile

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -104,7 +104,7 @@ export default function () {
                   d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
                 />
               </svg>
-              <Center w="100%">
+              <Center w="calc(100% - var(--chakra-space-8))">
                 <a href="/">
                   <img
                     src="/logo44.png"
