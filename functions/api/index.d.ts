d7d8cb56e6be1168e816d0a14b274c43427045df
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2022-11-06 20:50:58 +0000

    Create type definitions for public api endpoints

---
+++ functions/api/index.d.ts
@@ -0,0 +1,21 @@
+interface APIKey {
+  access_level: number;
+  created_at: number;
+  creator: string;
+}
+
+interface Env {
+  API_KEYS: KVNamespace;
+  INTERNAL_KEY: string;
+  VERIFICATIONS: KVNamespace;
+  [k: string]: string;
+}
+
+interface User {
+  id: number;
+  username: string;
+  privacy?: {
+    discord: number;
+    roblox: number;
+  };
+}
