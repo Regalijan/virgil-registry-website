1b65a544366b377c80ab42240f2dc492a789c7af
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2023-04-14 13:04:11 -0400

    Create banned component

---
+++ components/Banned.tsx
@@ -0,0 +1,16 @@
+import { Container, Heading, Link, Text } from "@chakra-ui/react";
+
+export default function (props: { email?: string }) {
+  return (
+    <Container maxW="container.md" textAlign="left">
+      <Heading size="4xl">0xB</Heading>
+      <br />
+      <br />
+      <Text fontSize="xl">This account is banned from the registry.</Text>
+      <br />
+      <Link color="red" href={"mailto:" + props.email}>
+        Reach out to discuss or appeal your ban
+      </Link>
+    </Container>
+  );
+}
