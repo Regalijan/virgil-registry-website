cac6fba0ef952871b6a5c3224c12f98c5e99397d
Regalijan <72576136+Regalijan@users.noreply.github.com>
2024-02-06 19:40:03 +0000

    Remove old future flags

--- remix.config.js
+++ remix.config.js
@@ -1,8 +1,4 @@
 export default {
-  future: {
-    v2_errorBoundary: true,
-    v2_routeConvention: true,
-  },
   server: "./server.ts",
   serverBuildPath: "functions/[[path]].js",
   serverMinify: true,
