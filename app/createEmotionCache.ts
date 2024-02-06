d7f034d81730b39d72932d1b4f9f568182af2b37
Regalijan <72576136+Regalijan@users.noreply.github.com>
2024-02-06 19:53:27 +0000

    Remove stupid hack

--- app/createEmotionCache.ts
+++ app/createEmotionCache.ts
@@ -1,10 +1,5 @@
 import createCache from "@emotion/cache";
 
 export default function createEmotionCache() {
-  // The browser throws when calling .default, but the server throws if we don't call .default
-  // Of course!
-  return typeof document === "undefined"
-    ? // @ts-expect-error
-      createCache.default({ key: "cha" })
-    : createCache({ key: "cha" });
+  return createCache({ key: "cha" });
 }
