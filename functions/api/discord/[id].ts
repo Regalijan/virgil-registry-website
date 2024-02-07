bbfb0ce1cdf77021020472564ba05646e694d154
Regalijan <72576136+Regalijan@users.noreply.github.com>
2024-02-07 03:11:06 +0000

    Support multiverification in get-by-discord endpoint

--- functions/api/discord/[id].ts
+++ functions/api/discord/[id].ts
@@ -3,25 +3,33 @@ import makeResponse from "../makeResponse";
 export async function onRequestGet(
   context: EventContext<Env, string, { [k: string]: any }>,
 ) {
-  const locatedUser: User | null = await context.env.VERIFICATIONS.get(
-    context.params.id as string,
-    "json",
-  );
+  const serverId = new URLSearchParams(context.request.url).get("server_id");
+
+  if (
+    serverId &&
+    (serverId.match(/\D/) || serverId.length > 19 || serverId.length < 17)
+  )
+    return makeResponse({ error: "Invalid Server ID" }, 400);
+
+  const locatedUser = await context.env.REGISTRY_DB.prepare(
+    "SELECT discord_privacy, roblox_id, username WHERE discord_id = ? AND server_id = ?;",
+  )
+    .bind(context.params.id, serverId)
+    .first();
+
   const { data } = context;
 
   if (!locatedUser) return makeResponse({ error: "User is not verified" }, 404);
 
-  const userPrivacy = locatedUser.privacy;
+  const userPrivacy = locatedUser.discord_privacy;
+  delete locatedUser.discord_privacy;
 
-  delete locatedUser.privacy;
-
-  if (!userPrivacy?.discord || data.is_internal)
-    return makeResponse(locatedUser, 200);
+  if (!userPrivacy || data.is_internal) return makeResponse(locatedUser, 200);
 
   if (!data.apiKeyInfo)
     return makeResponse({ error: "Resource requires API token" }, 401);
 
-  if (data.apiKeyInfo.access_level > userPrivacy.discord)
+  if (data.apiKeyInfo.access_level > userPrivacy)
     return makeResponse({ error: "You cannot access this user" }, 403);
 
   return makeResponse(locatedUser, 200);
