67a2d061f7d9aa863b774ccbe6133091d932aa1e
Regalijan <72576136+Regalijan@users.noreply.github.com>
2024-02-07 02:21:37 +0000

    Move unverification endpoint to D1

--- functions/client-api/linking/current.ts
+++ functions/client-api/linking/current.ts
@@ -1,23 +1,12 @@
 export async function onRequestDelete(
-  context: EventContext<
-    { [k: string]: string } & { VERIFICATIONS: KVNamespace },
-    string,
-    { [k: string]: any }
-  >,
+  context: EventContext<Env, string, { [k: string]: any }>,
 ) {
-  const verifyKV = context.env.VERIFICATIONS;
-  const verifyData = await verifyKV.get(context.data.user.id);
+  const db = context.env.REGISTRY_DB;
 
-  if (!verifyData)
-    return new Response(JSON.stringify({ error: "You are not verified" }), {
-      headers: {
-        "content-type": "application/json",
-      },
-      status: 404,
-    });
-
-  await verifyKV.delete(context.data.user.id);
-
+  await db
+    .prepare("DELETE FROM verifications WHERE discord_id = ?;")
+    .bind(context.data.user.id)
+    .run();
   await fetch(
     `https://discord.com/api/v10/users/@me/applications/${context.env.DISCORD_ID}/role-connection`,
     {
@@ -29,27 +18,6 @@ export async function onRequestDelete(
       method: "PUT",
     },
   );
-
-  const data = JSON.parse(verifyData);
-  const reverseData: string[] = JSON.parse(
-    (await verifyKV.get(data.id.toString())) ?? "[]",
-  );
-  const reverseIndex = reverseData.findIndex(
-    (id) => id === context.data.user.id,
-  );
-
-  if (reverseIndex === -1)
-    return new Response(null, {
-      status: 204,
-    });
-
-  reverseData.splice(reverseIndex, 1);
-
-  if (reverseData.length) {
-    await verifyKV.put(data.id.toString(), JSON.stringify(reverseData));
-  } else {
-    await verifyKV.delete(data.id.toString());
-  }
 
   return new Response(null, {
     status: 204,
