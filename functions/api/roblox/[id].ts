93a284d4a516f43704eaf1deebb49f7eb552aa5c
Regalijan <72576136+Regalijan@users.noreply.github.com>
2024-02-07 03:18:10 +0000

    Support multiverification in roblox-to-discord endpoint

--- functions/api/roblox/[id].ts
+++ functions/api/roblox/[id].ts
@@ -3,34 +3,32 @@ import makeResponse from "../makeResponse";
 export async function onRequestGet(
   context: EventContext<Env, string, { [k: string]: any }>,
 ) {
-  const locatedUsers: string[] | null = await context.env.VERIFICATIONS.get(
-    context.params.id as string,
-    "json",
-  );
+  const locatedUsers = (
+    await context.env.REGISTRY_DB.prepare(
+      "SELECT discord_id, roblox_privacy WHERE roblox_id = ?;",
+    )
+      .bind(context.params.id)
+      .all()
+  ).results;
   const { data } = context;
 
-  if (!locatedUsers)
+  if (!locatedUsers.length)
     return makeResponse(
       { error: "No Discord accounts linked to this Roblox account" },
       404,
     );
 
-  if (data.is_internal) return makeResponse(locatedUsers, 200);
+  if (data.is_internal)
+    return makeResponse(
+      locatedUsers.map((u) => u.roblox_id),
+      200,
+    );
 
   const usersToReturn = [];
 
-  for (const userId of locatedUsers) {
-    const user: User | null = await context.env.VERIFICATIONS.get(
-      userId,
-      "json",
-    );
-
-    if (!user) continue;
-
-    if ((user.privacy?.roblox as number) <= data.apiKeyInfo?.access_level) {
-      delete user.privacy;
-      usersToReturn.push(userId);
-    }
+  for (const user of locatedUsers) {
+    if ((user.roblox_privacy as number) <= data.apiKeyInfo?.access_level)
+      usersToReturn.push(user.discord_id);
   }
 
   if (!usersToReturn.length)
